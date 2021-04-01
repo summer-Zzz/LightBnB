4b3bd34ada7d049ee407554700592b3a64191aa6
Hongjin Zhu <summerellaooo@gmail.com>
2021-04-01 19:09:16 +0000

    updated getAllReservations function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -72,7 +72,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  const queryString = `
+  SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP by reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+  return pool.query (queryString, values)
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
